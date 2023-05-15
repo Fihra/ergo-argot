cf7f59e51e4f9bea7a4314219c71958547c4019e
fihra <fabrofabian@gmail.com>
2023-05-14 18:37:39 -1000

    created actions

---
+++ components/actions/actions.js
@@ -0,0 +1,10 @@
+ const ACTIONS = {
+    GET_DATA: "GET_DATA",
+    GET_LIST: "GET_LIST",
+    SET_WORD: "SET_WORD",
+    ADD_WORD: "ADD_WORD",
+    DELETE_WORD: "DELETE_WORD",
+    CLEAR_ALL: "CLEAR_ALL"
+}
+
+export default ACTIONS;
