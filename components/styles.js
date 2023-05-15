a581b3c9584f15f1ba56a53e6353201a549639dc
fihra <fabrofabian@gmail.com>
2023-05-15 00:57:09 -1000

    created styles file

---
+++ components/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        // paddingTop: 12,
+        backgroundColor: '#fff',
+        margin: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        marginLeft: 12,
+        marginRight: 12,
+      },
+      title: {
+        marginTop: 120,
+        fontSize: 24,
+        fontWeight: 'bold'
+      }
+});
+
+export default styles;
