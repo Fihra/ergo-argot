f7f21d4ad4f93ec38605a0af4986d1f205a00a05
fihra <fabrofabian@gmail.com>
2023-05-14 19:47:49 -1000

    using list from context

--- components/List.js
+++ components/List.js
@@ -1,19 +1,19 @@
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import useWord from '../context/WordContext';
+import WordCard from './WordCard';
 
-const List = (props) => {
+const List = () => {
     const { currentList } = useWord();
 
-    const testData = ["blahh", "blue", "waah"];
-
     const showList = () => {
-        return testData.map((word) => {
-            return <Text>{word}</Text>
+        return currentList.map((word) => {
+            return <WordCard word={word}/>
         })
     }
 
     return (
         <View style={{flex: 1, margin: 10}}>
+            <Text>Words Searched: {"\n"}</Text>
             {showList()}
         </View>
     )
