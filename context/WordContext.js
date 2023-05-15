0dd60430f0ed3118a4ffc237db6472f12dd5ca57
fihra <fabrofabian@gmail.com>
2023-05-14 18:37:56 -1000

    created word context

---
+++ context/WordContext.js
@@ -0,0 +1,45 @@
+import { createContext, useReducer, useContext } from 'react'
+import WordReducer, { initialState } from '../components/reducers/WordReducer'
+import ACTIONS from '../components/actions/actions';
+
+export const WordProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(WordReducer, initialState);
+
+    const fetchData = (data) => {
+        dispatch({
+            type: ACTIONS.GET_DATA,
+            payload: { data: data}
+        })
+    }
+
+    const setWord = (newWord) => {
+        dispatch({
+            type: ACTIONS.SET_WORD,
+            payload: { word: newWord}
+        })
+    }
+
+    const value = {
+        data: state.data,
+        currentList: state.currentList,
+        currentWord: state.currentWord,
+        setWord,
+        fetchData,
+    }
+
+    return <WordContext.Provider value={value}>{children}</WordContext.Provider>
+
+}
+
+const useWord = () => {
+    const context = useContext(WordContext);
+    if(context === undefined) {
+        throw new Error("no pattern context found");
+    }
+
+    return context;
+}
+
+export default useWord;
+
+export const WordContext = createContext(initialState);
