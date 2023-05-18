6be9fd6797d108395507fe80c518fd1e6521ced1
fihra <fabrofabian@gmail.com>
2023-05-17 23:26:26 -1000

    testing drawer setup

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View} from 'react-native';
 import styles from './components/styles';
 import React from 'react';
+import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
 import List from './components/List';
 import WordInput from './components/WordInput';
 import UrbanCard from './components/UrbanCard';
 import { WordProvider } from './context/WordContext';
+import 'react-native-gesture-handler';
 
 const App = () => {
   return (
 
       <View style={styles.container}>
         <StatusBar hidden={false}/>
+        <Topbar/>
         <Text style={styles.title}>Ergo Argot</Text>
         <WordProvider>
         <View>
@@ -26,3 +31,4 @@ const App = () => {
 }
 
 export default App;
+// registerRootComponent(App);
