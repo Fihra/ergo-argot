b6d5479fd481567073effd57658b8a22866caa19
fihra <fabrofabian@gmail.com>
2023-05-21 00:20:58 -1000

    added button styles

--- components/styles.js
+++ components/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
     container: {
         // paddingTop: 12,
-        backgroundColor: '#fff',
+        //backgroundColor: '#fff',
         margin: 0,
         justifyContent: 'center',
         alignItems: 'center',
@@ -12,9 +12,18 @@ const styles = StyleSheet.create({
         marginRight: 12,
       },
       title: {
-        marginTop: 120,
+        // marginTop: 120,
         fontSize: 24,
         fontWeight: 'bold'
+      },
+      button: {
+        width: 100,
+        height: 100,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 4,
+        // backgroundColor: 'red'
       }
 });
 
