9f24a5d602650d3754b76fb282953318738846a0
fihra <fabrofabian@gmail.com>
2023-05-22 23:12:53 -1000

    changed some styles

--- components/styles.js
+++ components/styles.js
@@ -2,28 +2,30 @@ import { StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        // paddingTop: 12,
-        //backgroundColor: '#fff',
+        position: "relative",
         margin: 0,
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
         marginLeft: 12,
         marginRight: 12,
+        
       },
       title: {
-        // marginTop: 120,
+        position: "absolute",
+        top: 30,
         fontSize: 24,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        color: "white",
       },
       button: {
         width: 100,
         height: 100,
         flex: 1,
+        paddingTop: 12,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 4,
-        // backgroundColor: 'red'
       }
 });
 
