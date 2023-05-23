b02af2b561ce1dac29b42d11e96990792d1e4562
fihra <fabrofabian@gmail.com>
2023-05-22 23:13:09 -1000

    added padding to line

--- components/Line.js
+++ components/Line.js
@@ -2,7 +2,7 @@ import { Text, View} from 'react-native';
 
 const Line = (props) => {
   return (
-    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+    <View style={{flexDirection: 'row', alignItems: 'center', padding: 20}}>
         <View style={{flex: 1, height: 1, backgroundColor: 'black'}}/>
     </View>
   )
