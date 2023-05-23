e5fd680b5f5b57380f304af42e26abd2ab77d4f7
fihra <fabrofabian@gmail.com>
2023-05-22 23:13:49 -1000

    added x icon next to word

--- components/WordCard.js
+++ components/WordCard.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const WordCard = (props) => {
     const { word } = props;
     return (
         <Text>
-            {word}
+            {word}<Text><FontAwesomeIcon icon={faX} style={{paddingRight: 12, color: "red"}}/></Text>
         </Text> 
     )
 };
