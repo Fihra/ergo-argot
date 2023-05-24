227fd20b21ab0f1a12e16b5798ed272596ecfbf1
fihra <fabrofabian@gmail.com>
2023-05-24 00:28:53 -1000

    added to change word from currentList

--- components/WordCard.js
+++ components/WordCard.js
@@ -2,19 +2,50 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import useWord from '../context/WordContext';
+import { REACT_APP_API_URL, REACT_APP_KEY, REACT_APP_HOST } from '@env';
+import axios from 'axios';
 
 const WordCard = (props) => {
-    const { deleteWord } = useWord();
+    const { currentList, fetchData, changeWord, deleteWord } = useWord();
     const { word } = props;
 
-    const handleClick = () =>{
-        console.log("Clicked item");
+    const handleClick = async (word) => {
+        const newWord = currentList.filter(w => {
+            return w === word
+        }).toString();
+
+        try {
+            const response = await axios.request(fetchWord(newWord));
+            if(response.data.list.length >= 1){
+                fetchData(response.data);
+            }
+        } catch(error) {
+            console.log(error);
+        }
+
+        changeWord(newWord);
+    }
+
+    const handleDelete = () =>{
         deleteWord(word);
+    }
+
+    const fetchWord = (word) => {
+        const wordOption = {
+          method: 'GET',
+          url: REACT_APP_API_URL,
+          params: {term: word},
+          headers: {
+            'X-RapidAPI-Key': REACT_APP_KEY,
+            'X-RapidAPI-Host': REACT_APP_HOST
+          }
+        }
+        return wordOption;
     }
 
     return (
         <Text>
-            {word}<Text onPress={handleClick}><FontAwesomeIcon icon={faX} style={{paddingRight: 12, color: "red"}}/></Text>
+            <Text onPress={() => handleClick(word)} value={word}>{word}</Text><Text onPress={handleDelete}><FontAwesomeIcon icon={faX} style={{paddingRight: 12, color: "red"}}/></Text>
         </Text> 
     )
 };
