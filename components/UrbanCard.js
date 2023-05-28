dcba7def4c063b594edde0d4998cabfe8e42644f
fihra <fabrofabian@gmail.com>
2023-05-27 22:27:16 -1000

    edited urban dictionary button and trying to add scroll text

--- components/UrbanCard.js
+++ components/UrbanCard.js
@@ -1,20 +1,25 @@
-import { Text, View, Button, Linking, Pressable, ScrollView } from 'react-native';
+import { Text, View, Button, Linking, Pressable, ScrollView, ToastAndroid } from 'react-native';
 import useWord from '../context/WordContext';
 import styles from './styles';
 
 const UrbanCard = () => {
     const { data } = useWord();
 
+    showMoreScroll = () => {
+        ToastAndroid.showWithGravity("Scroll for more", ToastAndroid.SHORT, ToastAndroid.TOP);
+      }
+
     const showCard = () => {
         if(data !== "") {
             return (
                 <ScrollView style={styles.urbanContainer}>
+                    {showMoreScroll()}
                     <View>
                     <Text>Word: <Text style={{fontWeight: "bold"}}>{data.list[0].word}</Text></Text>
                     <Text>Definition: {data.list[0].definition}</Text>
                     <Text>Example: {data.list[0].example}</Text>
                     <Pressable style={styles.button}>
-                    <Button onPress={() => Linking.openURL(data.list[0].permalink)} title="Link"/>
+                    <Button onPress={() => Linking.openURL(data.list[0].permalink)} title="Urban Dictionary"/>
                     </Pressable>
                     </View>
                 </ScrollView>
